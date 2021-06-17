cf7dd30e59626ee9e63939e4da01bfeeba0305a0
Melody <1606195172@qq.com>
2021-06-17 16:57:52 +0800

    Add files via upload

---
+++ UESTC-TeamWork-Software-engineering/software-engineering-web/Src/main/java/com/hhej789/softwareengineering/SoftwareEngineeringWebApplication.java
@@ -0,0 +1,13 @@
+package com.hhej789.softwareengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareEngineeringWebApplication.class, args);
+    }
+
+}
